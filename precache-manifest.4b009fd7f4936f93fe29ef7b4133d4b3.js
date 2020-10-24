3e7a2085cb11894ff6b293912544aec531f9b36a
Chris K <ikim1991@gmail.com>
2020-10-23 20:28:38 -0400

    Updates

---
+++ precache-manifest.4b009fd7f4936f93fe29ef7b4133d4b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f878db2774f1df434ceb4feec93da0f8",
+    "url": "/my-dashboard-app/index.html"
+  },
+  {
+    "revision": "991260066caf1d0fd114",
+    "url": "/my-dashboard-app/static/css/2.2967fba9.chunk.css"
+  },
+  {
+    "revision": "bd26230428307452cf64",
+    "url": "/my-dashboard-app/static/css/main.0e1dbfc8.chunk.css"
+  },
+  {
+    "revision": "991260066caf1d0fd114",
+    "url": "/my-dashboard-app/static/js/2.45a78752.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/my-dashboard-app/static/js/2.45a78752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd26230428307452cf64",
+    "url": "/my-dashboard-app/static/js/main.3f401c0a.chunk.js"
+  },
+  {
+    "revision": "04c7242d35215f0b2c76",
+    "url": "/my-dashboard-app/static/js/runtime-main.b817498d.js"
+  }
+]);
